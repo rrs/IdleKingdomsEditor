3188e899937e708aad415214b80caa762125be10
unknown <bobblezor@gmail.com>
2017-12-18 10:26:23 +0000

    added multiplier info for most tiles

--- IdleKingdomsEditor/MainWindow.xaml.cs
+++ IdleKingdomsEditor/MainWindow.xaml.cs
@@ -29,22 +29,58 @@ namespace IdleKingdomsEditor
             int total = 0;
             int forests = 0;
             double prestige = 1;
+            double food = 1;
+            double foodCart = 1;
+            double wood = 1;
+            double woodCart = 1;
+            double science = 1;
+            double scienceCart = 1;
 
             foreach(var hexItem in HexList.SelectedItems)
             {
                 total++;
 
                 var o = (DependencyObject)hexItem;
+
                 var type = TileHelper.GetTileType(o);
                 if (type == TileType.Forest) forests++;
-                var multiplier = TileHelper.GetPrestigeMultiplier(o);
-                if (multiplier > 1) prestige *= multiplier;
+
+                var pMultiplier = TileHelper.GetPrestigeMultiplier(o);
+                if (pMultiplier > 1) prestige *= pMultiplier;
+
+                var fMultiplier = TileHelper.GetFoodMultiplier(o);
+                if (fMultiplier > 1) food *= fMultiplier;
+
+                var fcMultiplier = TileHelper.GetFoodCartMultiplier(o);
+                if (fcMultiplier > 1) foodCart *= fcMultiplier;
+
+                var wMultiplier = TileHelper.GetWoodMultiplier(o);
+                if (wMultiplier > 1) wood *= wMultiplier;
+
+                var wcMultiplier = TileHelper.GetWoodCartMultiplier(o);
+                if (wcMultiplier > 1) woodCart *= wcMultiplier;
+
+                var sMultiplier = TileHelper.GetScienceMultiplier(o);
+                if (sMultiplier > 1) science *= sMultiplier;
+
+                var aMultiplier = TileHelper.GetAllCartMultiplier(o);
+                if (aMultiplier > 1)
+                {
+                    foodCart *= aMultiplier;
+                    woodCart *= aMultiplier;
+                    scienceCart *= aMultiplier;
+                }
             }
 
             SelectedInfo.Text = total.ToString();
             ForestInfo.Text = forests.ToString();
-            PrestigeInfo.Text = prestige.ToString();
-            
+            PrestigeInfo.Text = $"{prestige:N}";
+            FoodInfo.Text = $"{food:N}";
+            FoodCartInfo.Text = $"{foodCart:N}";
+            WoodInfo.Text = $"{wood:N}";
+            WoodCartInfo.Text = $"{woodCart:N}";
+            ScienceInfo.Text = $"{science:N}";
+            ScienceCartInfo.Text = $"{scienceCart:N}";
         }
 
         private void WidthTextBox_TextChanged(object sender, TextChangedEventArgs e)
--- IdleKingdomsEditor/TileHelper.cs
+++ IdleKingdomsEditor/TileHelper.cs
@@ -27,5 +27,89 @@ namespace IdleKingdomsEditor
         {
             obj.SetValue(PrestigeMultiplierProperty, value);
         }
+
+        public static readonly DependencyProperty FoodMultiplierProperty = DependencyProperty.RegisterAttached("FoodMultiplier", typeof(double), typeof(TileHelper));
+
+        public static double GetFoodMultiplier(DependencyObject obj)
+        {
+            return (double)obj.GetValue(FoodMultiplierProperty);
+        }
+
+        public static void SetFoodMultiplier(DependencyObject obj, double value)
+        {
+            obj.SetValue(FoodMultiplierProperty, value);
+        }
+
+        public static readonly DependencyProperty FoodCartMultiplierProperty = DependencyProperty.RegisterAttached("FoodCartMultiplier", typeof(double), typeof(TileHelper));
+
+        public static double GetFoodCartMultiplier(DependencyObject obj)
+        {
+            return (double)obj.GetValue(FoodCartMultiplierProperty);
+        }
+
+        public static void SetFoodCartMultiplier(DependencyObject obj, double value)
+        {
+            obj.SetValue(FoodCartMultiplierProperty, value);
+        }
+
+        public static readonly DependencyProperty AllCartMultiplierProperty = DependencyProperty.RegisterAttached("AllCartMultiplier", typeof(double), typeof(TileHelper));
+
+        public static double GetAllCartMultiplier(DependencyObject obj)
+        {
+            return (double)obj.GetValue(AllCartMultiplierProperty);
+        }
+
+        public static void SetAllCartMultiplier(DependencyObject obj, double value)
+        {
+            obj.SetValue(AllCartMultiplierProperty, value);
+        }
+
+        public static readonly DependencyProperty WoodMultiplierProperty = DependencyProperty.RegisterAttached("WoodMultiplier", typeof(double), typeof(TileHelper));
+
+        public static double GetWoodMultiplier(DependencyObject obj)
+        {
+            return (double)obj.GetValue(WoodMultiplierProperty);
+        }
+
+        public static void SetWoodMultiplier(DependencyObject obj, double value)
+        {
+            obj.SetValue(WoodMultiplierProperty, value);
+        }
+
+        public static readonly DependencyProperty WoodCartMultiplierProperty = DependencyProperty.RegisterAttached("WoodCartMultiplier", typeof(double), typeof(TileHelper));
+
+        public static double GetWoodCartMultiplier(DependencyObject obj)
+        {
+            return (double)obj.GetValue(WoodCartMultiplierProperty);
+        }
+
+        public static void SetWoodCartMultiplier(DependencyObject obj, double value)
+        {
+            obj.SetValue(WoodCartMultiplierProperty, value);
+        }
+
+        public static readonly DependencyProperty ScienceMultiplierProperty = DependencyProperty.RegisterAttached("ScienceMultiplier", typeof(double), typeof(TileHelper));
+
+        public static double GetScienceMultiplier(DependencyObject obj)
+        {
+            return (double)obj.GetValue(ScienceMultiplierProperty);
+        }
+
+        public static void SetScienceMultiplier(DependencyObject obj, double value)
+        {
+            obj.SetValue(ScienceMultiplierProperty, value);
+        }
+
+        public static readonly DependencyProperty ForagingHutMultiplierProperty = DependencyProperty.RegisterAttached("ForagingHutMultiplier", typeof(double), typeof(TileHelper));
+
+        public static double GetForagingHutMultiplier(DependencyObject obj)
+        {
+            return (double)obj.GetValue(ForagingHutMultiplierProperty);
+        }
+
+        public static void SetForagingHutMultiplier(DependencyObject obj, double value)
+        {
+            obj.SetValue(ForagingHutMultiplierProperty, value);
+        }
     }
 }
